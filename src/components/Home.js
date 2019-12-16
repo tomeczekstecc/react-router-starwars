58b6eb98aaee0ff89f2e3fe22716209a89dcd1bb
Tomasz <tomeczekstecc@gmail.com>
2019-12-16 20:21:10 +0100

    'first'

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <h1>Welcome to Star Wars Api</h1>
+      <h2>Explore one of the following characters:</h2>
+    </Fragment>
+  );
+};
+
+export default Home;
