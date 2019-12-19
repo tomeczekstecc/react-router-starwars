a350d2813dab153bb84b130d240e54168b7b9c66
Tomasz Steć <tomeczekstecc@gmail.com>
2019-12-19 22:21:50 +0100

    'search_bar'

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import {Link} from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -17,16 +18,58 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    }
   },
-  link:{
-textDecoration: 'none',
-color: 'white'
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200
+      }
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  inputRoot: {
+    color: 'inherit'
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto'
+    }
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'white'
   }
 }));
 
 const Nav = () => {
-   const classes = useStyles();
+  const classes = useStyles();
   return (
     <div className={classes.root}>
       <AppBar position='static' color='primary'>
@@ -42,13 +85,26 @@ const Nav = () => {
           <Typography variant='h6' className={classes.title}>
             StarWarsApi
           </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder='Search…'
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
           <Button color='inherit'>
-            <Link className={classes.link}  to='/characters'>
+            <Link className={classes.link} to='/characters'>
               Characters
             </Link>
           </Button>
           <Button color='inherit'>
-            <Link className={classes.link}  to='/'>
+            <Link className={classes.link} to='/'>
               Home
             </Link>
           </Button>
